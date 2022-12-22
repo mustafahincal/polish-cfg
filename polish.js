7418479f01d97b1c2108272f6e6a5c2b5afe37f5
mustafahincal <mustafahncal@gmail.com>
2022-12-22 18:13:52 +0300

    add propmt

--- polish.js
+++ polish.js
@@ -1,34 +1,32 @@
-import prompt from "prompt"
+import prompt from "prompt";
 
 // const input = "* + * + 1 2 + 3 4 5 6";
 let stack = [];
 let firstItem, secondItem;
 
-const {input} = await  prompt.get("input");
+const { input } = await prompt.get("input");
 
 const inputArray = input.split(" ").reverse();
 
-const calculate = (operator,x,y) => {
-    if(operator === "+"){
-        return x + y;
-    }else if(operator === "-"){
-        return x - y;
-    }else if(operator === "*"){
-        return x * y;
-    }else if(operator === "/"){
-        return x / y;
-    }
-
-}
-
+const calculate = (operator, x, y) => {
+  if (operator === "+") {
+    return x + y;
+  } else if (operator === "-") {
+    return x - y;
+  } else if (operator === "*") {
+    return x * y;
+  } else if (operator === "/") {
+    return x / y;
+  }
+};
 inputArray.forEach((item) => {
-    if(!isNaN(Number(item))){
-        stack.push(Number(item));
-    }else{
-        firstItem = stack.pop();
-        secondItem = stack.pop();
-        stack.push(calculate(item,firstItem,secondItem));
-    }
-})
+  if (!isNaN(Number(item))) {
+    stack.push(Number(item));
+  } else {
+    firstItem = stack.pop();
+    secondItem = stack.pop();
+    stack.push(calculate(item, firstItem, secondItem));
+  }
+});
 
 console.log(stack);

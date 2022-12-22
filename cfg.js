80f284575be04ea62b92ee377e21b1cd9e3457e0
mustafahincal <mustafahncal@gmail.com>
2022-12-22 18:13:41 +0300

    add multiple variable configuration

--- cfg.js
+++ cfg.js
@@ -1,44 +1,56 @@
-import prompt from "prompt"
+import prompt from "prompt";
 
 const E = "a,b,X";
-const S = "aa|bX|aXX";
+const S = "aa|bX|aXY";
 const X = "ab|b";
+const Y = "b|aa";
+const bigLetters = ["X", "Y"];
 
 // const {E,S,X} = await  prompt.get(["E","S","X"]);
 
-let SPaths = S.split("|");
-let XPaths = X.split("|");
+const paths = {
+  S: S.split("|"),
+  X: X.split("|"),
+  Y: Y.split("|"),
+};
 
 let createdWord;
 let createdWords = [];
 let repeatedWords = [];
 let control = 0;
 
-
 const createWord = (expression) => {
-    if(expression.includes("X")){
-        XPaths.forEach((XPath) => {
-            createdWord = expression.replace("X",XPath);
-            if(createdWord.includes("X")){
-                control = 1;
-                createWord(createdWord);
-            }else{
-                if(control == 1){
-                    repeatedWords.push(createdWord);
-                }
-                createdWords.push(createdWord);
-                
+  if (bigLetters.some((bigLetter) => expression.includes(bigLetter))) {
+    bigLetters.forEach((letter) => {
+      if (expression.includes(letter)) {
+        paths[letter].forEach((path) => {
+          createdWord = expression.replace(letter, path);
+          if (bigLetters.some((bigLetter) => createdWord.includes(bigLetter))) {
+            control = 1;
+            createWord(createdWord);
+          } else {
+            if (control == 1) {
+              if (!repeatedWords.find((word) => word === createdWord)) {
+                repeatedWords.push(createdWord);
+              }
             }
-                
-        })  
-    }else{
-        createdWords.push(expression);
+            if (!createdWords.find((word) => word === createdWord)) {
+              createdWords.push(createdWord);
+            }
+          }
+        });
+      }
+    });
+  } else {
+    if (!createdWords.find((word) => word === expression)) {
+      createdWords.push(expression);
     }
-}   
+  }
+};
 
-SPaths.forEach((SPath) => {
-    createWord(SPath);
-})
+paths.S.forEach((SPath) => {
+  createWord(SPath);
+});
 
 console.log("Üretilen Kelimeler : " + createdWords);
 console.log("Tekrarlanan Kelimeler : " + repeatedWords);
